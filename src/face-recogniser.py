f72b9863a97c585f8c90c6c4452274d3496c9586
MariaLombardi <maria.lombardi1@iit.it>
2022-02-07 14:35:37 +0100

    evaluate accuracy on test set

--- src/face-recogniser.py
+++ src/face-recogniser.py
@@ -78,19 +78,20 @@ class FaceRecogniser(yarp.RFModule):
         return True
 
     def respond(self, command, reply):
-        print(command)
-        print(command.size())
-        print(command.get(0).asString())
-        print(command.get(1).asString())
-        print(command.get(2).asString())
-        #command = command.get(0).asList()
         if command.get(0).asString() == 'quit':
             self.TRAIN = 0
             self.cleanup()
             reply.addString('Quit command sent')
         elif command.get(0).asString() == 'train':
             self.TRAIN = 1
-            reply.addString("Training started.")
+            # init the models again
+            self.dataset = []
+            self.svm_model = None
+            self.encoder = None
+            self.normaliser = None
+            self.name_file = command.get(1).asString()
+            self.face_selected = command.get(2).asString()
+            reply.addString('Training started. Files will be saved as ' + self.name_file + '. Face of interest: ' + self.face_selected)
         elif command.get(0).asString() == 'init':
             self.TRAIN = 0
             # init the models again
@@ -98,8 +99,7 @@ class FaceRecogniser(yarp.RFModule):
             self.svm_model = None
             self.encoder = None
             self.normaliser = None
-            self.name_file = command.get(1).asString()
-            reply.addString('Init. Files will be saved as ' + self.name_file)
+            reply.addString('Init done.')
         elif command.get(0).asString() == 'run':
             self.TRAIN = 0
             # load svm model
@@ -140,97 +140,106 @@ class FaceRecogniser(yarp.RFModule):
 
             received_data = self.in_port_human_data.read()
             if received_data:
-                poses, conf_poses, faces, conf_faces = read_openpose_data(received_data)
-                # images 160x160 pixels
-                # faces are ordered from left to right (x-axis increasing)
-                if poses and len(poses) <= len(self.labels_set):
-                    faces_img, bboxes, order = extract_faces(human_image, poses, required_size=(160, 160))
-                    for i in range(0, len(faces_img)):
-                        cv2.imshow('image id %d' % i, cv2.cvtColor(faces_img[i], cv2.COLOR_BGR2RGB))
-                        cv2.waitKey(1)
-                    # print bounding box
-                    human_image = draw_bboxes(human_image, bboxes)
-
-                    # if TRAINING collect the dataset
-                    if self.TRAIN == 1 and len(poses) == len(self.labels_set):
-                        # suppose the sequence of people from left to right is always the same
-                        # idx 0: therapist, id 1: child
-                            for i in range(0, len(faces_img)):
-                                self.dataset.append((get_embedding(self.facenet_model, faces_img[i]), self.labels_set[i]))
-
-                            # train each 50 samples
-                            if len(self.dataset) % (50*2) == 0:
-                                trainX = np.squeeze(np.asarray([data[0] for data in self.dataset]))
-                                trainy = np.asarray([data[1] for data in self.dataset])
-
-                                in_encoder = Normalizer(norm='l2')
-                                trainX = in_encoder.transform(trainX)
-
-                                out_encoder = LabelEncoder()
-                                out_encoder.fit(trainy)
-                                trainy = out_encoder.transform(trainy)
-
-                                base_model = SVC(kernel='rbf', probability=True, class_weight="balanced")
-                                base_model.fit(trainX, trainy)
-                                model = CalibratedClassifierCV(base_estimator=base_model, cv="prefit")
-                                model.fit(trainX, trainy)
-
-                                yhat_train = model.predict(trainX)
-                                # score
-                                score_train = accuracy_score(trainy, yhat_train)
-                                print('Dataset length: %d ; Accuracy: train=%.3f' % (len(self.dataset), score_train * 100))
-
-                                self.svm_model = model
-                                self.normaliser = in_encoder
-                                self.encoder = out_encoder
-
-                                if len(self.dataset) > 300*2 and score_train > 0.99:
-                                    pk.dump(self.svm_model, open(self.output_path_models + 'svm_model_' + self.name_file + '.pkl', 'wb'))
-                                    pk.dump(self.encoder, open(self.output_path_models + 'label_encoder_model_' + self.name_file + '.pkl', 'wb'))
-                                    pk.dump(self.normaliser, open(self.output_path_models + 'normaliser_model_' + self.name_file + '.pkl', 'wb'))
-                                    pk.dump(self.dataset, open(self.output_path_datasets + 'dataset_' + self.name_file + '.pkl', 'wb'))
-                                    print("Training done. Models have been saved.")
-                                    self.TRAIN = 0
-
-                    # here everything is none, init phase
-                    if self.svm_model is not None and self.encoder is not None and self.normaliser is not None:
-                        # prediction for the face
-                        data = []
+                try:
+                    poses, conf_poses, faces, conf_faces = read_openpose_data(received_data)
+                    # images 160x160 pixels
+                    # faces are ordered from left to right (x-axis increasing)
+                    if poses and len(poses) <= len(self.labels_set):
+                        faces_img, bboxes, order = extract_faces(human_image, poses, required_size=(160, 160))
                         for i in range(0, len(faces_img)):
-                            data.append(get_embedding(self.facenet_model, faces_img[i]))
+                            cv2.imshow('image id %d' % i, cv2.cvtColor(faces_img[i], cv2.COLOR_BGR2RGB))
+                            cv2.waitKey(1)
+                        # print bounding box
+                        human_image = draw_bboxes(human_image, bboxes)
 
-                        data = self.normaliser.transform(data)
-                        data_array = np.asarray(data)
-                        yhat_class = self.svm_model.predict_proba(data_array)
-                        for itP in range(0, yhat_class.shape[0]):
-                            # get name
-                            prob = max(yhat_class[itP])
-                            y_pred = (np.where(yhat_class[itP] == prob))[0]
+                        # if TRAINING collect the dataset
+                        if self.TRAIN == 1 and len(poses) == len(self.labels_set):
+                            # suppose the sequence of people from left to right is always the same
+                            # idx 0: therapist, id 1: child
+                                for i in range(0, len(faces_img)):
+                                    self.dataset.append((get_embedding(self.facenet_model, faces_img[i]), self.labels_set[i]))
 
-                            predicted_name = self.encoder.inverse_transform(y_pred)
+                                # train each 50 samples (first time I'll get 100)
+                                if len(self.dataset) > 50*2 and len(self.dataset) % (50*2) == 0:
+                                    trainX = np.squeeze(np.asarray([data[0] for data in self.dataset[:-50]]))
+                                    trainy = np.asarray([data[1] for data in self.dataset[:-50]])
+                                    testX = np.squeeze(np.asarray([data[0] for data in self.dataset[-50:]]))
+                                    testy = np.asarray([data[1] for data in self.dataset[-50:]])
 
-                            txt = "%s" % predicted_name[0]
-                            human_image = cv2.putText(human_image, txt, tuple([int(bboxes[itP][0]), int(bboxes[itP][3])+20]), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 0), 2, cv2.LINE_AA)
-                            txt = "c: %0.1f" % prob
-                            human_image = cv2.putText(human_image, txt, tuple([int(bboxes[itP][0]), int(bboxes[itP][3])+50]), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 0), 2, cv2.LINE_AA)
+                                    in_encoder = Normalizer(norm='l2')
+                                    trainX = in_encoder.transform(trainX)
+                                    testX = in_encoder.transform(testX)
 
-                        if self.TRAIN == 0:
-                            # send in output the selected pose from openpose as bottle
-                            face_selected_idx = self.encoder.transform(np.asarray([self.face_selected]))
-                            choice_idx = np.argmax(yhat_class[face_selected_idx, :])
-                            openpose_idx = (int)(order[choice_idx])
-                            pred = yarp.Bottle()
-                            pred.addList().read((received_data.get(0).asList()).get(openpose_idx))
-                            pred_list = yarp.Bottle()
-                            pred_list.addList().read(pred)
-                            self.out_port_prediction.write(pred_list)
+                                    out_encoder = LabelEncoder()
+                                    out_encoder.fit(trainy)
+                                    trainy = out_encoder.transform(trainy)
+                                    testy = out_encoder.transform(testy)
 
-                            selected_pose = poses[openpose_idx]
-                            centroid = compute_centroid([selected_pose[joint] for joint in JOINTS_POSE if joint_set(selected_pose[joint])])
-                            human_image = cv2.circle(human_image, tuple([int(centroid[0]), int(centroid[1])]), 6, (0, 0, 255), -1)
+                                    base_model = SVC(kernel='rbf', probability=True, class_weight="balanced")
+                                    base_model.fit(trainX, trainy)
+                                    model = CalibratedClassifierCV(base_estimator=base_model, cv="prefit")
+                                    model.fit(trainX, trainy)
 
-                else:
-                    print("Warning! Human faces detected > labels. Human faces: " + str(len(poses)))
+                                    yhat_train = model.predict(trainX)
+                                    yhat_test = model.predict(testX)
+
+                                    # score
+                                    score_train = accuracy_score(trainy, yhat_train)
+                                    score_test = accuracy_score(testy, yhat_test)
+                                    print('Dataset length: %d ; Accuracy: train=%.3f - test=%.3f' % (len(self.dataset), score_train, score_test))
+
+                                    self.svm_model = model
+                                    self.normaliser = in_encoder
+                                    self.encoder = out_encoder
+
+                                    if len(self.dataset) > 300*2 and score_train > 0.99 and score_test > 0.99:
+                                        pk.dump(self.svm_model, open(self.output_path_models + 'svm_model_' + self.name_file + '.pkl', 'wb'))
+                                        pk.dump(self.encoder, open(self.output_path_models + 'label_encoder_model_' + self.name_file + '.pkl', 'wb'))
+                                        pk.dump(self.normaliser, open(self.output_path_models + 'normaliser_model_' + self.name_file + '.pkl', 'wb'))
+                                        pk.dump(self.dataset, open(self.output_path_datasets + 'dataset_' + self.name_file + '.pkl', 'wb'))
+                                        print("Training done. Models have been saved.")
+                                        self.TRAIN = 0
+
+                        # here everything is none, init phase
+                        if self.svm_model is not None and self.encoder is not None and self.normaliser is not None:
+                            # prediction for the face
+                            data = []
+                            for i in range(0, len(faces_img)):
+                                data.append(get_embedding(self.facenet_model, faces_img[i]))
+
+                            data = self.normaliser.transform(data)
+                            data_array = np.asarray(data)
+                            yhat_class = self.svm_model.predict_proba(data_array)
+                            for itP in range(0, yhat_class.shape[0]):
+                                # get name
+                                prob = max(yhat_class[itP])
+                                y_pred = (np.where(yhat_class[itP] == prob))[0]
+
+                                predicted_name = self.encoder.inverse_transform(y_pred)
+
+                                txt = "%s" % predicted_name[0]
+                                human_image = cv2.putText(human_image, txt, tuple([int(bboxes[itP][0]), int(bboxes[itP][3])+20]), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 0), 2, cv2.LINE_AA)
+                                txt = "c: %0.1f" % prob
+                                human_image = cv2.putText(human_image, txt, tuple([int(bboxes[itP][0]), int(bboxes[itP][3])+50]), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 0), 2, cv2.LINE_AA)
+
+                            if self.TRAIN == 0:
+                                # send in output the selected pose from openpose as bottle
+                                face_selected_idx = self.encoder.transform(np.asarray([self.face_selected]))
+                                choice_idx = np.argmax(yhat_class[face_selected_idx, :])
+                                openpose_idx = (int)(order[choice_idx])
+                                pred = yarp.Bottle()
+                                pred.addList().read((received_data.get(0).asList()).get(openpose_idx))
+                                pred_list = yarp.Bottle()
+                                pred_list.addList().read(pred)
+                                self.out_port_prediction.write(pred_list)
+
+                                selected_pose = poses[openpose_idx]
+                                centroid = compute_centroid([selected_pose[joint] for joint in JOINTS_POSE if joint_set(selected_pose[joint])])
+                                human_image = cv2.circle(human_image, tuple([int(centroid[0]), int(centroid[1])]), 6, (0, 0, 255), -1)
+                    else:
+                        print("Warning! Human faces detected > labels. Human faces: " + str(len(poses)))
+                except Exception as err:
+                    print("Unexpected error!!! " + err)
 
             txt = "TRAINING: %d, NAME: %s" % (self.TRAIN, self.name_file)
             human_image = cv2.putText(human_image, txt, tuple([20, 450]), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2, cv2.LINE_AA)
